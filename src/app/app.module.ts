98033d55fc59587dd8021ccc24441fe134e5c200
ernane <ernanecarvalho89@gmail.com>
2021-10-14 09:03:47 -0300

    Definindo rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
-import { ProductsPageComponent } from './products-page/products-page.component';
-import { PetsPageComponent } from './pets-page/pets-page.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { FramePageComponent } from './pages/master/frame.page';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +21,7 @@ import { CartPageComponent } from './cart-page/cart-page.component';
     SignupPageComponent,
     ProductsPageComponent,
     PetsPageComponent,
-    CartPageComponent
+    FramePageComponent
   ],
   imports: [
     BrowserModule,
